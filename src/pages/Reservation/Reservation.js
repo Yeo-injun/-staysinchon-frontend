dadf6f90c3a4293896f5ed086346529d2be229b4
Yeo-injun <youth.com11@gmail.com>
2021-07-15 23:33:49 +0900

    Reservation.css 수정

--- src/pages/Reservation/Reservation.js
+++ src/pages/Reservation/Reservation.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import './Reservation.css'; // css 적용
 
 import React, { useState, useEffect } from 'react'
 import moment from "moment";
@@ -10,74 +11,88 @@ import { Button } from '../../globalStyles';
 import axios from 'axios';
 
 
-const Reservation = () => {
+function Reservation() {
     
+    /************** 전역 변수 *******************/
     const { RangePicker } = DatePicker;
     const dateFormat = 'YYYY/MM/DD';
+    /************** 전역 변수 [끝] *******************/
 
-    const config = {
-        headers: { 'Authorization' : 'Bearer ${ }' },
-    }
 
-    let [ roomData, setRoomData ] = useState(null); 
+    /************** useState *******************/ 
+    // useState 초기값 설정이 중요함
+    // 변환되는 자료형이 배열이면 초기값도 배열로 반영
+    let [ roomData, setRoomData ] = useState([]); 
     let [ isOk, setIsOk ] = useState(false);
+    /************** useState [끝] **************/ 
 
+
+    /************** useEffect ************************/ 
     // room 데이터 받아오기 : useEffect() 안에서 Axios로 API서버 호출
     // useEffect 실행시기 1. 컴포넌트 로딩이 끝난 후(mount 완료) 2. 컴포넌트 재렌더링 끝난 후(update 완료)
     useEffect(()=> {
-        console.log('실행?');
+        getRoomList();
+    }, []); // useEffect()의 두번째 인자는 useEffect 호출횟수를 지정( []값이 있을때는 1번만 호출 )
+    /************** useEffect [끝] *******************/ 
+
+    /* 방 목록 기본값 호출 */
+    function getRoomList() {
         axios.get('http://localhost:8080/rooms')
             .then((result)=>{ 
-                console.log("____________");
-                setRoomData(result.data);
-                console.log("____________");
-                console.log(result.Authorization);                        
+                var rs = result.data;                
+                setRoomData(rs);                     
             })
             .catch(()=>{ '요청실패시실행할코드' })
-    }, []); // useEffect()의 두번째 인자는 useEffect 호출횟수를 지정( []값이 있을때는 1번만 호출 )
+    }
 
-
-    /* HTML덩어리 */
-    return (
-        <div>
+  /************** HTML 화면 영역 **************/ 
+  return (
+        <div className="contents">
             <ImageSlider slides={SliderData} />
-            <div className = 'datepicker' style = {{textAlign: 'center'}} />
-            Available Bedrooms
-            <br/>
+            <div className ="title" >
+                <h3>Available Rooms</h3>
+            </div>
             <br/>
 
             {/* DatePicker */}
-            <Space direction="vertical" size={12}>
-                <RangePicker 
-                defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
-                format={dateFormat} />
-            </Space>
+            <div className = "datepicker">
+                <Space direction="vertical" size={12}>
+                    <RangePicker 
+                    defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+                    format={dateFormat} />
+                </Space>
 
-            {/* onClick 메소드로 검색 호출 */}
-            <Button  style = {{marginTop: '15px'}}>Search</Button>
-            
+                {/* [구현예정] onClick 메소드로 검색 호출 */}
+                <Button  style = {{margin: '10px'}}>Search</Button>
+            </div>
+
             {// RommData 뿌려주기 
             roomData.map(
                 (room) => { 
                     console.log(room);
                     return (
-                    <div className="room">
+                    <div className="roomItem">
                         <img src=""/>
                         <span>방 이름</span> {/** DB컬럼 추가 요망 : 방이름 */}
-                        <span><Button>Book This Room</Button></span>
                         <p>Room Type : { room.room_type }</p>
                         <p>Accommodate : { room.capacity }</p>
                         <p>Beds : { room.bed }</p>
                         <p>Bathroom : { room.bathroom }</p>
-                        <br/>
-                        <p>Daily Price : { room.price_day }</p>
-                        <p>Monthly Price : { room.price_day }</p>
+
+                        <div className="priceContent">
+                            <li>Daily Price : { room.price_day }</li>
+                            <li>Monthly Price : { room.price_day }</li>
+                        </div>
+
+                        <div className="button">
+                            <Button>Book This Room</Button>
+                        </div>
                     </div>
-                )}
+               )}
             )
             }
         </div>
-    )
+    ) /* HTML덩어리 [끝] */
 }
 
 export default Reservation
