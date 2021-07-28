7989ae1f8cb78f38243206a7bb9e8049287b7789
Yeo-injun <youth.com11@gmail.com>
2021-07-28 23:35:12 +0900

    MyPage 화면 생성

---
+++ src/pages/Mypage/MyPage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+
+function MyPage() {
+
+    return (
+        <div>
+            마이페이지여~!
+        </div>
+
+    );
+}
+
+export default MyPage
--- src/pages/UserPages.js
+++ src/pages/UserPages.js
@@ -2,12 +2,21 @@ import React from 'react';
 import '../App.css';
 import GlobalStyle from '../globalStyles'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import { Navbar } from '../components';
+
 import Home from '../pages/HomePage/Home';
+
+// 예약을 위한 화면
 import Reservation from '../pages/Reservation/Reservation'
 import ReservationForm from '../pages/Reservation/ReservationForm'
-import { Navbar } from '../components';
+
+// 로그인, 회원가입을 위한 화면
 import Signup from '../components/Form/Signup';
 import Register from '../components/Form/Register';
+
+// 게스트의 예약 관리 화면
+import MyPage from '../pages/Mypage/MyPage';
 
 function UserPages() {
     return (
@@ -20,7 +29,8 @@ function UserPages() {
           <Route exact path='/reservation' component = {Reservation} />
           <Route exact path='/reservation/form' component = {ReservationForm} />
           <Route exact path='/signup' component={Signup} />
-          <Route exact path="/register" component={Register} /> 
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/mypage" component = {MyPage} /> 
         </Switch>
       </Router>
     );
