361b99d0bec559dbb25b744778257694f3855de4
Yeo-injun <youth.com11@gmail.com>
2021-07-29 00:07:49 +0900

    Mypage 사용자별 예약이력 조회 Axios GET통신 구현중

--- src/pages/Mypage/MyPage.js
+++ src/pages/Mypage/MyPage.js
@@ -1,10 +1,44 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react'
 
 function MyPage() {
+    /************** useState *******************/ 
+    let [ data, setData ] = useState(''); // 임시용
+    /************** useState [끝] *******************/ 
 
+    
+    /************** useEffect ************************/ 
+    useEffect(()=> {
+        getMyReservationList();
+    }, []);
+    /************** useEffect [끝] *******************/ 
+
+    // 사용자별 예약이력 데이터 가져오기    
+    function getMyReservationList() {
+        axios.get(
+            "http://localhost:8080/reservations"
+            , { headers : { 
+                'Authorization': localStorage.getItem("authToken") 
+              }
+        })
+        .then(function(response) {
+            setData(response.data);
+            console.log(data);
+        })
+    }
     return (
         <div>
             마이페이지여~!
+            {/* {data.map(
+                (obj) => {
+
+                    return (
+                        <>
+                            {obj}
+                        </>
+                    
+                )}
+            )} */}
         </div>
 
     );
