c7913613ea2da2c783cc83db1eaa155a3a590bea
Yeo-injun <youth.com11@gmail.com>
2021-07-30 00:36:43 +0900

    MyPage 데이터 뿌려주기 구현 완료 (기본 css 적용)

--- src/pages/Mypage/MyPage.js
+++ src/pages/Mypage/MyPage.js
@@ -1,5 +1,8 @@
+import '../common-component.css';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { Button } from '../../globalStyles';
 
 function MyPage() {
     /************** useState *******************/ 
@@ -32,18 +35,50 @@ function MyPage() {
         })
         .catch(()=>{ '요청실패시실행할코드' })
     }
-    
-    return (
-        <div>
-            마이페이지여~!
-            {myReservationList.map(
-                (reservation) => {
 
+    function dateFormater(date) {
+        let dateFormat = new Date(date);
+        let yyyy = dateFormat.getFullYear();
+        let mm = dateFormat.getMonth();
+        let dd = dateFormat.getDate();
+
+        let result = yyyy+'-'+mm+'-'+dd;
+        return result;
+
+    }
+
+    return (
+        <div className="contents">
+            <h2>My Profile</h2>
+
+            <h3>-----------------------------</h3>
+            <h2>My Reservations</h2>
+            your total reservation : {myReservationList.length}
+
+            {myReservationList.map(
+                (res) => {
+                    /* 예약이력 목록*/
                     return (
-                        <>
-                            {reservation.res_ID}
-                        </>
-                    
+                        <div className="item">
+                            Image -- 사진 추가하기...!
+                            <div>{res.res_date}</div>
+                            <div> Reservation ID : {res.res_ID} </div>
+                            <div> Room Name : {res.room_ID} Room Name으로 바꾸기</div> 
+                            <div> Check in : {dateFormater(res.check_in)} </div> 
+                            <div> Check out : {dateFormater(res.check_out)} </div>
+                            <div> Guests : {res.num_of_guests} </div>
+                            <div> Purpose for stay : {res.stay_purpose} </div>
+                            <div> Message : {res.message} </div>
+                            <div> Payment : {res.payment} </div>
+                            
+                            <div>
+                                {res.state} State 값에 따라서 상태 출력(예약신청 - 예약확정 - 예약취소 - 숙박중 - 숙박완료) 
+                                State값에 따라 버튼 3개 만들기
+                                위 사항들을 하나의 자식 컴포넌트로 만들어 주기 : res.State값을 props로 넘겨줌
+                            </div>    
+
+                        </div>
+
                 )}
             )}
         </div>
