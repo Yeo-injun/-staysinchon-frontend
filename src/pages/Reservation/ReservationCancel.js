46c4369d8b9cfb2c982943d4074306e8aef4b9a7
Yeo-injun <youth.com11@gmail.com>
2021-08-02 20:45:15 +0900

    ReservationState.js 버튼 클릭시 모달창 팝업 구현 완료

---
+++ src/pages/Reservation/ReservationCancel.js
@@ -0,0 +1,22 @@
+import './Reservation.css'; // css 적용
+
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'; // 특정경로로 이동시키기 위한 함수
+
+import 'antd/dist/antd.css';
+import { Form, Input, InputNumber, Button, Cascader, Radio} from 'antd';
+
+import axios from 'axios';
+
+
+function ReservationCancel(){
+
+
+    return(
+        <div>
+            취소 모달!!
+        </div>
+    )
+}
+
+export default ReservationCancel
--- src/pages/Reservation/ReservationState.js
+++ src/pages/Reservation/ReservationState.js
@@ -5,29 +5,53 @@ import React, {useState, useEffect, useRef} from 'react';
 
 import axios from 'axios';
 
+import { Modal } from 'antd';
 import { Button } from '../../globalStyles';
 
+import UpdateModal from './ReservationUpdate';
+import CancelModal from './ReservationCancel';
+
 function ReservationState(props) {
-    
+    /************** useState영역 *******************/ 
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    let [ isModalVisible, setIsModalVisible ] = useState(false); // Modal 상태
 
     let [curState, setCurState] = useState('');
     let [isDisableBtn, setIsDisableBtn] = useState(true);
 
+    let [isUpdate, setIsUpdate] = useState(true); // 취소버튼 누를경우 false
+    /************** useState영역 [끝] **************/ 
+
+    /************** useEffect 영역 ************************/ 
     useState(()=>{
         setCurState(checkState(props.state, props.checkIn, props.checkOut));
-        console.log('------------------');
-        console.log(curState);
-        console.log(typeof(props.state));
     });
+    /************** useEffect 영역 [끝] *******************/ 
 
-    // 예약 상태 반환 함수 
-    /*
-    State 값에 따라서 상태 출력(예약신청 - 예약확정 - 예약취소 - 숙박중 - 숙박완료) 
-            예약신청 : state값 1
-            예약취소 : state값 3
-            숙박완료 : state값 2 + checkOut 날짜 이후
-            숙박중 : state값 2 + checkIn 날짜 이후 + checkOut 날짜 전
-            예약확정 : state값 2 + checkIn 날짜 전            
+    const handleClick = () => setClick(!click);  // 기본값이 false
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+
+
+    /* 예약 상태 반환 함수 
+        State 값에 따라서 상태 출력(예약신청 - 예약확정 - 예약취소 - 숙박중 - 숙박완료) 
+        예약신청 : state값 1
+        예약취소 : state값 3
+        숙박완료 : state값 2 + checkOut 날짜 이후
+        숙박중 : state값 2 + checkIn 날짜 이후 + checkOut 날짜 전
+        예약확정 : state값 2 + checkIn 날짜 전            
     */
     function checkState(state, checkIn, checkOut) {
         let now = uniDateType(new Date());
@@ -73,8 +97,17 @@ function ReservationState(props) {
         return uniType;
     }
 
+
+    // 예약 취소하기 API 호출
     function cancelReservation() {
-        alert("경고창!");
+        setIsUpdate(false); // 모달창 띄울때 구분자
+        setIsModalVisible(true);
+    }
+
+    // 예약 수정하기 API 호출
+    function updateReservation() {
+        setIsUpdate(true); // 모달창 띄울때 구분자
+        setIsModalVisible(true);
     }
 
     return (
@@ -91,13 +124,27 @@ function ReservationState(props) {
                     disabled={isDisableBtn}>
                 Cancel
             </Button> 
+
             {/* 숙박종료, 예약취소, 숙박중 상태면 비활성화 */}
             <Button className={(curState == 'apply' || curState == 'confirm') ? "btn" : "btn disabled"}
-                    onClick={cancelReservation}
+                    onClick={updateReservation}
                     disabled={isDisableBtn}>
                 Update
             </Button>
-       
+
+            {/* 예약취소 또는 예약수정 버튼 클릭시 모달창 팝업 */}
+            <Modal 
+                title={(isUpdate) ? "Update Reservation" : "Cancel Reservation"} 
+                visible={isModalVisible} 
+                onOk={handleOk} 
+                onCancel={handleCancel}
+                footer={null}>
+                {/* cancel 또는 update에 따라 다른 태그 보여주기 */}
+                {(isUpdate) 
+                    ? <UpdateModal/> 
+                    : <CancelModal/>} 
+            </Modal>
+
         </div>
     );
 }
